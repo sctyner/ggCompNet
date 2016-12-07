90fb06234fd2fbc4596317ac03279a2df791669d
Heike Hofmann <hofmann@iastate.edu>
2016-12-07 15:47:03 -0600

    speed comparison vignette is up and running

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Runtime results from protein example
+#' @format a data frame consisting of 100 comparison runs across five different packages
+#' \itemize{
+#'   \item iteration iteration number
+#'   \item igraph runtime under the igraph package
+#' }
+"runtimes_protein"
+
+
+#' Runtime comparison
+#'
+#' @format much bigger data set
+#' \itemize{
+#'   \item list all variables
+#' }
+"compare_all"
